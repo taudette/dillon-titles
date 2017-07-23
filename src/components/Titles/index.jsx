9abe3a43f2328008b59ffbef555eb1ff656b4f3e
Tyler Audette <tyleraudette5@gmail.com>
2017-07-23 14:07:27 -0600

    Update css

--- src/components/Titles/index.jsx
+++ src/components/Titles/index.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
 
 const renderTitle = (title) => {
-  console.log(title)
+
   let imagePath = 'https://d2snwnmzyr8jue.cloudfront.net/{artKey}_270.jpeg'.replace('{artKey}', title.artKey)
   return(
-    <div key={title.titleId}>
-      <img className="image" src={imagePath} alt={title.title} height="300" width="207"/>
-      <p>
-        {title.title}<br/>
-        {title.artistName ? title.artistName : 'artist unknown'}
-      </p>
-    </div>
+    <li key={title.titleId}>
+     
+        <img className="image" src={imagePath} alt={title.title} />
+      <div className="details">
+        <h3>{title.title ? title.title : 'title unknown'}</h3>
+        <p className="image-author">{title.artistName ? title.artistName : 'artist unknown'}</p>
+      </div>
+    </li>
   )
 }
 
-
 const TitlesComponent = (props) => {
   return (
-    <div className="images">
-      <section>
+    <div>
+      <ul className="image-list-small">
         { props.titles.map(title => renderTitle(title)) }
-      </section>
+      </ul>
     </div>
   )
 }
