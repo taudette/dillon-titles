85099b88b8e4da476f8404040076e0f3706127b8
Tyler Audette <tyleraudette5@gmail.com>
2017-07-23 10:23:28 -0600

    Add with credentials to request

--- src/actions.js
+++ src/actions.js
@@ -2,13 +2,12 @@ import * as types from './actionTypes';
 import axios from 'axios';
 //import { pushState } from 'redux-react-router';
 
-const webService = 'https://hoopla-ws-dev.hoopladigital.com/kinds/7/titles/featured?offset=0&limit=51&kindId=7'
-
 function requestData() {
   return {type: types.REQ_DATA}
 };
 
 function receiveData(json) {
+  console.log('recieved: ', json)
   return{
     type: types.RECV_DATA,
     data: json
@@ -16,27 +15,35 @@ function receiveData(json) {
 };
 
 function receiveError(json) {
+  console.log('error: ', json)
   return {
     type: types.RECV_ERROR,
     data: json
   }
 };
 
+
+
+
 export function fetchData(url) {
-  console.log('fetch')
+  console.log('fetch: ', url)
   return function(dispatch) {
     dispatch(requestData());
     return axios({
+      //method: 'post',
       url: url,
+      withCredentials: true,
       timeout: 20000,
-      method: 'get',
-      header: 'ws-api',
-      value: '2.1'
+
+      headers: {'ws-api': '2.1'},
+      //TODO: make these variables
     })
       .then(function(response) {
+        console.log(response)
         dispatch(receiveData(response.data));
       })
       .catch(function(response){
+        console.log(response)
         dispatch(receiveError(response.data));
       //  dispatch(pushState(null,'/error'));
       })
